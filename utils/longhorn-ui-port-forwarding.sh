542ba7bcbf96866c43d593a034c5919465231c74
lamtnguyen989 <lamtnguyen989@gmail.com>
2025-07-14 19:58:40 -0500

    fixing scripts

--- utils/longhorn-ui-port-forwarding.sh
+++ utils/longhorn-ui-port-forwarding.sh
@@ -1,3 +1,3 @@
 # Script for launching longhorn (storage) WebUI via  port-forwarding (will delete in the future tho)
 xdg-open https://localhost:8081
-kubectl port-forward service/argocd-server -n argocd 8081:80
+kubectl port-forward svc/longhorn-frontend -n longhorn-system 8081:80
