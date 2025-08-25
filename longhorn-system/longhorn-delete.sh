179fe3317f9e6b6e16f61b54eff77f8f2629b8d8
Lam Nguyen <lamtnguyen989@gmail.com>
2025-08-25 15:20:37 -0500

    organizing

--- longhorn-system/longhorn-delete.sh
+++ longhorn-system/longhorn-delete.sh
@@ -1,4 +1,16 @@
+# Longhorn namespace remove finalizers
+NAMESPACE=longhorn-system
+kubectl get namespace $NAMESPACE -o json > $NAMESPACE.json
+sed -i -e 's/"kubernetes"//' $NAMESPACE.json
+kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f ./$NAMESPACE.json
+
+# Delete CRDs round 1
 kubectl get crd | grep 'longhorn.io' | awk '{print $1}' | xargs kubectl delete crd
+# Delete CRDs round 2 for stuck resources due to finalizers
+kubectl get crd -o name | grep 'longhorn.io' | while read crd; do
+    kubectl patch "$crd" -p '{"metadata":{"finalizers":[]}}' --type=merge
+done
+
 kubectl delete ClusterRoleBinding longhorn-bind && kubectl delete ClusterRoleBinding longhorn-support-bundle
 kubectl delete ClusterRole longhorn-role
-kubectl delete namespace longhorn-system
+kubectl delete namespace $NAMESPACE
