88f4dcf304a68969dfa2cc32624af766f8a156a4
Lam Nguyen <lamtnguyen989@gmail.com>
2025-08-26 15:26:19 -0500

    housekeeping

--- longhorn-system/longhorn-delete.sh
+++ longhorn-system/longhorn-delete.sh
@@ -3,6 +3,7 @@ NAMESPACE=longhorn-system
 kubectl get namespace $NAMESPACE -o json > $NAMESPACE.json
 sed -i -e 's/"kubernetes"//' $NAMESPACE.json
 kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f ./$NAMESPACE.json
+rm longhorn-system.json
 
 # Delete CRDs round 1
 kubectl get crd | grep 'longhorn.io' | awk '{print $1}' | xargs kubectl delete crd
--- utils/longhorn-ui-port-forwarding.sh
+++ utils/longhorn-ui-port-forwarding.sh
@@ -1,3 +1,3 @@
 # Script for launching longhorn (storage) WebUI via  port-forwarding (will delete in the future tho)
-xdg-open https://localhost:8081
+xdg-open http://localhost:8081/#/dashboard
 kubectl port-forward svc/longhorn-frontend -n longhorn-system 8081:80
