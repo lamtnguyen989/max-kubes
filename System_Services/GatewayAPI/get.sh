fdfcfe13ffb223c33d2b45cb0004068454a98bef
Lam Nguyen <lamtnguyen989@gmail.com>
2025-10-27 19:39:46 -0500

    deploying metrics-server

--- System_Services/GatewayAPI/get.sh
+++ System_Services/GatewayAPI/get.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Importing the source link and extract the version
-VERSION=$(echo "$(cat source.txt)" | grep -o "v[0-9]\+\.[0-9]\+\.[0-9]\+")
+LINK=$(cat source.txt)
+VERSION=$(echo $LINK | grep -o "v[0-9]\+\.[0-9]\+\.[0-9]\+")
 
 # Download the source yaml file
-curl -sSL $(cat source.txt) -o "gateway-api-standard-$VERSION.yaml"
+curl -sSL $LINK -o "gateway-api-standard-$VERSION.yaml"
